ae734fbcb22c788aec5bb5f3c69e8e951102f5a7
Zouzow <enzo.bermond@etu.unice.fr>
2026-05-14 01:48:49 +0200

    Starting page (empty)

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script type="module" src="./assets/js/app.js"></script>
+  <title>Blueprint Generator</title>
+</head>
+<body>
+  <h1>Blueprint Generator</h1>
+  <form action="">
+    
+  </form>
+</body>
+</html>
